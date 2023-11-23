475286ae01af3ba39c79f118fc62ada4e6e90ada
virtualabishek <717abishekneupane@gmail.com>
2023-11-23 18:58:45 +0545

    Begin

---
+++ TC1.cpp
@@ -0,0 +1,16 @@
+//Sample Calculator
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+    cout<<"enter the second number"<<endl;
+    cin>>b;
+    cout<<"Sum is: "<<(a+b)<<endl;
+    cout<<"Sub is: "<<(a-b)<<endl;
+    cout<<"Mul is: "<<(a*b)<<endl;
+    return 0;
+}
