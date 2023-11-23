fe3d4241a93e52111df1f924342d39345f42f421
virtualabishek <717abishekneupane@gmail.com>
2023-11-24 05:06:52 +0545

    DMA

--- DMA.cpp
+++ DMA.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
-int main() {
-int *intptr;
-float *floatptr;
-floatptr = new float;
-intptr = new int;
-*floatptr = 45.5;
-*intptr = 50;
-cout<<*floatptr<<endl;
-cout<<*intptr<<endl;
-delete floatptr;
-delete intptr;
+int main() 
+{
+    int *intptr;
+    float *floatptr;
+    floatptr = new float;
+    intptr = new int;
+    *floatptr = 45.5;
+    *intptr = 50;
+    cout<<*floatptr<<endl;
+    cout<<*intptr<<endl;
+    delete floatptr;
+    delete intptr;
     return 0;
 }
