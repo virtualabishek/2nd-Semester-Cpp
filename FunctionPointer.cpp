f7eaf90cc1813d4113d8fe6473a9f57b8dc25ab6
virtualabishek <717abishekneupane@gmail.com>
2023-11-25 05:41:36 +0545

    Pointer Function

---
+++ FunctionPointer.cpp
@@ -0,0 +1,13 @@
+//Pointer and Functions
+#include<iostream>
+using namespace std;
+const int MAX = 3;
+int main() {
+int arr[MAX]={1,2,3};
+int *ptr = arr;
+for (int i=0; i<3; i++) {
+    cout<<"The value for ['"<<i<<"'] is "<<*ptr<<endl;
+    ptr++;
+}
+    return 0;
+}
