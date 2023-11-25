be269b9251aaedac5ac873dad4c90d9b88d8e17f
virtualabishek <717abishekneupane@gmail.com>
2023-11-25 09:15:21 +0545

    feri

--- DMFOutside.cpp
+++ DMFOutside.cpp
@@ -1,7 +1,35 @@
+//Defining member function of class.
 #include<iostream>
 using namespace std;
+class myClass {
+    private:
+    int length, breadth;
+    public:
+    void setData(int l, int b) {
+        length=l;
+        breadth=b;
+    }
+    void showdata();
+    int findarea();
+    int findperi();
+};
+void myClass::showdata() {
+    cout<<"Length: "<<length<<endl;
+    cout<<"Breadth: "<<breadth<<endl;
+}
+int myClass::findarea() {
+    return length*breadth;
+}
+int myClass::findperi() {
+    return (2*length+breadth);
+}
 
 int main() {
-    
+    myClass ob;
+    ob.setData(2,3);
+    ob.showdata();
+    cout<<"Area is: "<<ob.findarea()<<endl;
+    cout<<"Peri is: "<<ob.findperi()<<endl;
+
     return 0;
 }
